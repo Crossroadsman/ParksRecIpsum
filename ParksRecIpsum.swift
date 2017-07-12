8af2e5c02fed1bebaed8f619ae5f8bfd1ea26e66
Alex Koumparos <crossroadsman@gmail.com>
2017-07-11 20:08:38 -0600

    Create ParksRecIpsum.swift

---
+++ ParksRecIpsum.swift
@@ -0,0 +1,182 @@
+class ParksRecIpsum {
+    
+    //MARK: - Properties
+    //------------------
+    private let tom_s1e1 = ["Problem is anything over $25 I have to report.", "So, uh, maybe give my wife a call and give her the suits, and then if they don't fit her maybe she'll give them to me."]
+    private let tom_s1e2 = ["Uh-oh."]
+    private let tom_s1e6 = ["Oh my god, your boobs are dead!"]
+    private let tom_s2e2 = ["Whenever Leslie asks me for the Latin names of any of our plants, I just give her the name of rappers.", "Uh, those are some Diddies.", "There's some Bonethugs and Harmoniums right there.", "Those Ludacrises are coming in great.", "Yeah, you know, Leslie, the best way to figure out what kind of spice that is, is to roll it up in a joint and smoke it.", "No, I'm from South Carolina.", "My mother's uterus.", "Wow, no, I was conceived in America.", "My parents are Indian.", "My birth name is Darwish Zubair Ismail Gani, then I changed it to Tom Haverford because, you know, brown guys with funny-sounding Muslim names don't make it far into politics.", "Okay, yeah, fine, Barack Obama.", "If I'd known a guy named Barack Obama was gonna be elected president, yeah, maybe I wouldn't have changed it."]
+    private let tom_s2e4 = ["Well, if you're squeaky clean like I am, there's no problem.", "Yeah, but I've never sealed the deal, just window shopping.", "You can fly to Brazil, just don't enter the cave, am I right?", "Hey Donna, let me ask you something: do you hate black people?", "'Cause apparently, in 1998, you donated money to the presidential campaign for... David Duke!"]
+    private let tom_s2e3 = ["Hey, John McClane!", "Die Hard."]
+    private let tom_s2e8 = ["When Tiger Woods feels invincible, he wears a red shit and black pants.", "Ron wears the same thing after he's had sex."]
+    private let tom_s2e9 = ["I have no interest in art.", "Let me clarify: I have no interest in non-nude images."]
+    private let tom_s2e10 = ["You can't say your favorite kind of cake is birthday cake, that's like saying your favorite kind of cereal is breakfast cereal.", "On a scale of 1 to Chris Brown, how pissed is he?", "Maybe Ron shot himself."]
+    private let tom_s2e11 = ["What?", "Leslie, I appreciate what you're trying to do, but once you go in there, you will see what you cannot unsee.", "Alright."]
+    private let tom_s2e12 = ["Yeah, nobody believes that garbage.", "Nobody believes that thing."]
+    private let tom_s2e14 = ["Dammit, got to re-foliate!", "Justin is hip, Pawnee is the opposite of hip.", "People in this town are just now getting into Nirvana.", "I don't have the heart to tell them what's going to happen to Kurt Cobain in 1994."]
+    private let tom_s2e19 = ["Actually, seven is the worldwide average."]
+    private let tom_s2e20 = ["Modern life: where are we running?", "Sometimes, what we want is not always where we are.", "Are we alone?", "Is the real winter inside or hearts?", "We are all struggling for definition in a world that resists our increase.", "Girl, you're more precious than Precious.", "Nice hat, wanna bone?", "That's you talking to me."]
+    private let tom_s2e22 = ["You stayed up all night, the night before an all-night telethon?"]
+    private let tom_s2e23 = ["You know the old rule: half your age, plus seven.", "Twenty...", "...One.", "...And a half.", "Yep.", "Please, you totally can!"]
+    
+    
+    private let ron_s1e1 = [
+        "I’ve been quite open about this around the office.",
+        "I don’t want this parks department to build any parks, because I don’t believe in government.",
+        "I think that all government is a waste of tax payer money.",
+        "My dream is to have the parks system privatized and run entirely for profit by corporations, like Chuck E. Cheese.",
+        "They have an impeccable business model.",
+        "I would rather work for Chuck E. Cheese.",
+        "I’ve created this office as a symbol of how I feel about government.",
+        "This sawed off shotgun belonged to a local bootlegger.",
+        "People who come in here to ask me for things have to stare right down the barrel.",
+        "This is my basketball court.",
+        "I don’t want to see any double dribble, I don’t want to see any three second violations."]
+    private let ron_s1e2 = [
+        "There’s a new wind blowing in government, and I don’t like it.",
+        "All of a sudden there’s all this federal money coming in, and Paul the City Manager is telling us to build parks, start new community programs.",
+        "It's horrifying."
+    ]
+    private let ron_s1e3 = [
+        "Tommy Boy!",
+        "Let me tell you something, Tom, you suck at Scrabble.",
+        "You’re worse than my ex-wife, and she’s terrible at Scrabble; and she’s a bitch.",
+        "Yeah I doubt that.",
+        "Her name is Tammy Swanson and she’s a serious bitch.",
+        "I'm not an idiot: I know Tom has been losing to me on purpose.",
+        "But I like Tom: he doesn't do a lot of work around here, he shows zero initiative, he's not a team player, he's never one to go the extra mile.",
+        "Tom is exactly what I'm looking for in a government employee."
+    ]
+    private let ron_s1e4 = [
+        "No, it’s not, this is not communist China: you can not make her whip herself, you can not make her wear a hairshirt.",
+        "This is America.",
+        "You wanna live in North Korea, you can live in North Korea.",
+        "I don't want to, I wanna to live in America",
+        "Leslie has never broken a rule in her life to the point that it's annoying.",
+        "If you wanna slap her on the wrist, go ahead.",
+        "If you wanna do anything more serious, you're gonna have to go through me.",
+        "Let's go.",
+        "No, I didn't... I didn't do it for Leslie, I did it because I hate bureaucracy.",
+        "My idea of a perfect government is one guy who sits in a small room at a desk, and the only thing he's allowed to decide is who to nuke.",
+        "The man is chosen based on some kind of IQ test, and maybe also a physical tournament, like a decathlon.",
+        "And women are brought to him, maybe, when he desires them."
+    ]
+    private let ron_s1e5 = [
+        "The only reason anyone is going to this thing is because they're afraid of what Marlene will do to them if they don't.",
+        "That woman is tough.",
+        "In 1994, I gave her a nickname: it's unrepeatable, but it stuck, it's my proudest accomplishment.",
+        "It's The Iron Cock-Shredder of Pawnee.",
+        "I enjoy government functions like I enjoy getting kicked in the nuggets with a steel-toed boot.",
+        "But this hotel always serves bacon-wrapped shrimp.",
+        "That's my number-one favorite food wrapped around my number-three favorite food.",
+        "I'd go to a banquet in honor of those Somali pirates if they served bacon-wrapped shrimp.",
+        "Excuse me.",
+        "I like your hairdo, Leslie.",
+        "It's just like my brother's; he's an officer in the Air Force.",
+        "Bacon-wrapped shrimp.",
+        "I wasn't offering.",
+        "I refuse to lather Marlene up, kiss her ring like everybody else.",
+        "Instead, I'll be delivering a speech of facts.",
+        "Marlene is a woman.",
+        "She has worked in government for three decades.",
+        "Thirty years, properly applied, that's how long a good varnish should last.",
+        "So, Marlene, it is true that you have won this award."
+    ]
+    private let ron_s1e6 = [
+        "Hey Mark, this is Beth, my ex-wife Tammy's better-looking sister.",
+        "Yep, my ex-wife Tammy cheated on me, then we divorced, then last week I ran into her sister, Beth, here.",
+        "Turns out, she hates Tammy, too, so we started dating.",
+        "It's like a fairytale.",
+        "Hey, who wants to see me climb a tree?"
+    ]
+    
+    private let ron_s2e1 = ["toDo"]
+    private let ron_s2e2 = ["toDo"]
+    
+    //MARK: - Typealias
+    //-----------------
+    
+    typealias Person = [[[String]]]
+    
+    let tom: Person
+    let ron: Person
+    
+    //MARK: - Initialisers
+    //--------------------
+    
+    init() {
+        // Load Persons
+        tom = [
+            [tom_s1e1, tom_s1e2, tom_s1e6], [tom_s2e2,tom_s2e3, tom_s2e4, tom_s2e8, tom_s2e9, tom_s2e10, tom_s2e11, tom_s2e12, tom_s2e14, tom_s2e19, tom_s2e20, tom_s2e22, tom_s2e23], []
+        ]
+        
+        ron = [
+            [ron_s1e1,ron_s1e2,ron_s1e3,ron_s1e4,ron_s1e5,ron_s1e6],[ron_s2e1,ron_s2e2]
+        ]
+    }
+    
+    
+    //MARK: - Methods
+    //---------------
+    
+    /**
+     Returns a list of all sentences for the specified person from the supplied seasons
+     e.g., sentencePool(forPerson: ron, seasons: [1, 2, 4]) will return all Ron sentences from seasons 1, 2, and 4
+     */
+    private func getSentences(forPerson person: Person, seasons: [Int] = [1]) -> [String] {
+        //print("person: \(person) <END PERSON>")
+        var sentences = [String]()
+        
+        for season in seasons {
+            //print("season: \(season) <END SEASON>")
+            if season > seasons.count {
+                //print("season \(season) > season count (\(seasons.count))")
+                continue
+            }
+            for episode in person[season - 1] {
+                //print("season \(season): \(person[season - 1]) <END SEASON (episodes)>")
+                for sentence in episode {
+                    sentences.append(sentence)
+                }
+            }
+        }
+        return sentences
+    }
+    
+    /**
+     returns a paragraph using random sentences from the supplied sentence list and with number of sentences equal to the specified length
+     */
+    private func makeParagraph(fromSentenceList sentenceList: [String], withLength totalSentences: Int = 5) -> String {
+        
+        var paragraph: String = ""
+        var sentencesSoFar = 0
+        while sentencesSoFar < totalSentences {
+            let rand = Int(arc4random_uniform(UInt32(sentenceList.count)))
+            paragraph.append("\(sentenceList[rand]) ")
+            sentencesSoFar += 1
+        }
+        return paragraph
+    }
+    
+    func makeIpsum(forPerson person: Person, seasons: [Int] = [1], paragraphs: Int = 1, sentences: Int = 5) -> String {
+        
+        guard paragraphs > 0 && sentences > 0 else { return "" }
+        
+        var output = ""
+        
+        let sentencePool = getSentences(forPerson: person, seasons: seasons)
+        
+        guard sentencePool.count > 0 else { return "" }
+        
+        for i in 0 ..< paragraphs {
+            let para = makeParagraph(fromSentenceList: sentencePool, withLength: sentences)
+            output += "\(para)"
+            if i < paragraphs - 1 {
+                output += "\n"
+            }
+        }
+        
+        return output
+    }
+    
+}
